fc00fcff413abc85b5cb4b1caedec7c773a17125
SvelteRust <oddharald@myhren.ai>
2025-08-27 12:16:03 +0300

    Best font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import "@master/css";
+  import "@fontsource/geist-sans";
 
   // Props
   const { children } = $props();
@@ -8,3 +9,9 @@
 <main class="container mt:2em">
   {@render children()}
 </main>
+
+<style>
+  :root {
+    --pico-font-family: Geist;
+  }
+</style>
